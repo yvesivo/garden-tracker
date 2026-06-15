f535f85b6779727f841763b68712d7c35ebbf357
yvesivo <ivo.adam.ivo@gmail.com>
2026-06-15 14:46:47 +0200

    chore: create backend maven-project with spring-boot dependencies

---
+++ backend/src/main/java/com/gardentracker/GardenTrackerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gardentracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GardenTrackerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GardenTrackerBackendApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/gardentracker/GardenTrackerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gardentracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GardenTrackerBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
